b69448d9a84511dbf24df425fa779123f1874cec
SowIbra24 <ibrahima.sow0608@gmail.com>
2024-12-29 18:57:20 +0100

    ajout de la fonction decale_ligne qui decale les lignes de la grille de gauche à droite, tests ok

--- logique_jeu.php
+++ logique_jeu.php
@@ -1,6 +1,24 @@
 <?php
     require_once('fonctions_jeu.php');
 
-?>
+    function decale_ligne(&$grille,$indLigne, $direction) {
+        $resultat = array_fill(0, 4, 0);
+        if ($direction === "gauche") {
+            $indexe = 0; 
+            foreach ($grille[$indLigne] as $valeur) {
+                if ($valeur != 0) {
+                    $resultat[$indexe++] = $valeur;
+                }
+            }
+        } else { 
+            $indexe = 3;
+            foreach (array_reverse($grille[$indLigne]) as $valeur) {
+                if ($valeur != 0) {
+                    $resultat[$indexe--] = $valeur;
+                }
+            }
+        }
+        $grille[$indLigne] = $resultat;
+    }
     
- 
+?>
